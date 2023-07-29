307a2492e771989242facc6bb9312afe38f0ec3f
iswarning <vinh@DESKTOP-V9NAGSI>
2023-07-29 13:42:06 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:7000',
-      'https://main--courageous-parfait-f124e4.netlify.app/',
-      'http://www.example.com',
-      'http://app.example.com',
-      'https://example.com',
-      'https://www.example.com',
-      'https://app.example.com',
-    ],
-    methods: ["GET", "POST"],
+    origin: "*",
     credentials: true,
   });
+  await app.listen(5000)
 }
 bootstrap();
